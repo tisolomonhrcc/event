46c4236555bca0a0258e23ffd6f0f7092cacb82f
Kola <theoton100@gmail.com>
2026-03-27 12:00:15 +0000

    Fix mobile centering: use flex center and shrink-0 to keep oversized image centered

--- app/page.tsx
+++ app/page.tsx
@@ -25,15 +25,15 @@ export default function Home() {
           </p>
         </div>
 
-        {/* Character Image - 2/3 of previous mobile size and centered */}
-        <div className="flex justify-center relative z-10 w-full">
+        {/* Character Image - 200% width on mobile, perfectly centered */}
+        <div className="flex justify-center items-center relative z-10 w-full">
           <Image
             src="/image2.png"
             alt="Welcome Character"
             width={800}
             height={320}
             priority
-            className="w-[200%] max-w-none md:w-[250%] h-auto object-contain relative left-1/2 -translate-x-1/2 md:left-0 md:translate-x-0"
+            className="w-[200%] max-w-none md:w-[250%] h-auto object-contain shrink-0"
           />
         </div>
 
